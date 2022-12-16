d05b681a840d4de45b78985a0e1cfc5b426315f3
developerdesigner18 <developer.designer18@gmail.com>
2022-12-16 11:16:28 +0530

    vercel deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const product = require("./api/product");
 app.use(express.json());
 app.use(
     express.urlencoded({
@@ -8,8 +8,6 @@ app.use(
     })
 );
 
-app.get("/", (req, res) => {
-    res.send("Root API");
-});
+app.use("/api", product);
 
 app.listen(8000, () => console.log("PORT 8000"));
